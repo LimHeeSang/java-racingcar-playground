c6c041ec0ed23f35854edcbf542ef29168768ea3
heesang <dlagmltkd12@naver.com>
2022-03-19 05:57:31 +0900

    feat(racingcar) : 자동차 이름을 입력받는 기능 구현

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public List<String> introduce() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String carNames = scanner.nextLine();
+        String[] splitedCarNames = carNames.split(",");
+        return Arrays.asList(splitedCarNames);
+    }
+}
